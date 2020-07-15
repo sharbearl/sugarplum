d4fa7150231a9642ee1abdfc3ffcb1bb5f6d7331
Sharon <68297755+sharbearl@users.noreply.github.com>
2020-07-15 11:36:39 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var playerX, playerY;
 
 function setup(){
   createCanvas(1100, 500);
-  background(255);
+  background(255, 237, 247);
   frameRate(100);
   GameOn = true;
   playerX = playerY = 0;
@@ -13,7 +13,7 @@ function draw(){
   if(GameOn = true){
     noStroke();
     fill(255, 156, 228);
-    ellipse(550, 490, 30, 30);
+    ellipse(550, 480, 30, 30);
   }
   else{
   }
